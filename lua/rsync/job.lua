985cada7ae3ddb03272e2b73d9b28b1815424e6d
Michel Garcia <michelgarxia@gmail.com>
2023-08-10 23:46:42 -0400

    feat: create job module

---
+++ lua/rsync/job.lua
@@ -0,0 +1,98 @@
+local M = {}
+M.current = nil
+M.__index = M
+
+M.id = 0
+M.config = {
+    exclude = {
+        ".rsync.*"
+    },
+    include = {}
+}
+M.status = "starting"
+M.percentage = 0
+
+M.new = function (config)
+    local self = setmetatable({}, M)
+    self.config = vim.tbl_deep_extend("force", M.config, config)
+    return self
+end
+
+M.prepare = function (self)
+    local opts = {
+        "--archive",
+        "--info=progress2",
+        "--verbose"
+    }
+    if self.config.delete then
+        table.insert(opts, "--delete")
+    end
+    for _, pattern in ipairs(self.config.exclude) do
+        local opt = string.format("--exclude %s", pattern)
+        table.insert(opts, opt)
+    end
+    for _, pattern in ipairs(self.config.include) do
+        local opt = string.format("--include %s", pattern)
+        table.insert(opts, opt)
+    end
+    if self.config.port then
+        local opt = string.format("--rsh='ssh -p %s'", self.config.port)
+        table.insert(opts, opt)
+    end
+    local args = table.concat(opts, " ")
+    self.cmd = string.format(
+        "rsync %s %s %s", args, self.config.src, self.config.dest
+    )
+    if self.config.pass then
+        self.cmd = string.format(
+            "sshpass -p '%s' %s", self.config.pass, self.cmd
+        )
+    end
+end
+
+M.start = function (self)
+    local opts = {
+        capture_stdout = true,
+        on_exit = function (_, code)
+            self:on_exit(code)
+        end,
+        on_stdout = function (_, data)
+            self:on_stdout(data)
+        end
+    }
+    if not self:prepare() then
+        return false
+    end
+    self.id = vim.fn.jobstart(self.cmd, opts)
+    local active = self.id > 0
+    if active then
+        M.current = self
+    end
+    return active
+end
+
+M.stop = function (self)
+    return vim.fn.jobstop(self.id)
+end
+
+M.on_exit = function (self, code)
+    M.current = nil
+end
+
+M.on_stdout = function (self, data)
+    self.status = "starting"
+    for _, line in ipairs(data) do
+        if line ~= "" then
+            local percentage = line:match("(%d+)%%")
+            if percentage ~= nil then
+                self.percentage = tonumber(percentage)
+            end
+        end
+    end
+end
+
+return setmetatable(M, {
+    __call = function (_, ...)
+        return M.new(...)
+    end
+})
