824f417d483bfc1fee0da03ce74a8aad2a0a2d23
Michel Garcia <michelgarxia@gmail.com>
2023-08-17 16:55:25 -0400

    feat: create events module

---
+++ lua/rsync/events.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+local prev = nil
+
+M.progress = function (job)
+    local message = string.format("Syncing: %s%%", job.percentage)
+    local opts = {
+        timeout = false,
+        title = "Rsync",
+        replace = prev
+    }
+    prev = vim.notify(message, "info", opts)
+end
+
+M.complete = function (job)
+    local opts = {
+        timeout = 3500,
+        title = "Rsync",
+        replace = prev
+    }
+    local message = "Sync complete!"
+    vim.notify(message, "success", opts)
+end
+
+M.error = function (job)
+    local opts = {
+        title = "Rsync",
+        replace = prev
+    }
+    local message = "error"
+    vim.notify(message, "error", opts)
+end
+
+return M
