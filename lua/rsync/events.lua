f10b4f37471ba3b4125fa1283ef458e9d6e91238
Michel Garcia <michelgarxia@gmail.com>
2023-08-23 15:45:15 -0400

    fix(notifications): show notifications normally if there is not a previous one to replace

--- lua/rsync/events.lua
+++ lua/rsync/events.lua
@@ -6,27 +6,35 @@ M.progress = function (job)
     local message = string.format("Syncing: %s%%", job.percentage)
     local opts = {
         timeout = false,
-        title = "Rsync",
-        replace = prev
+        title = "Rsync"
     }
+    if prev then
+        opts.replace = prev
+    end
     prev = vim.notify(message, "info", opts)
 end
 
 M.complete = function (job)
     local opts = {
         timeout = 3500,
-        title = "Rsync",
-        replace = prev
+        title = "Rsync"
     }
+    if prev then
+        opts.replace = prev
+        prev = nil
+    end
     local message = "Sync complete!"
     vim.notify(message, "success", opts)
 end
 
 M.error = function (job)
     local opts = {
-        title = "Rsync",
-        replace = prev
+        title = "Rsync"
     }
+    if prev then
+        opts.replace = prev
+        prev = nil
+    end
     local Errors = require("rsync.errors")
     local error = Errors.get_error(job.code)
     local message = string.format("Rsync error: %s", error)
